79b40f97ce51c2cd5dea4182ec69828908c03db1
ThunderRabbit <zuzazibubabi@gmail.com>
2024-12-26 21:03:32 +0800

    added local storage functionality

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from "react";
 
 export default function ToDo() {
   const [testCase, setTestCase] = useState(false);
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState(() => {
+    const savedToDos = localStorage.getItem("toDos");
+    return savedToDos ? JSON.parse(savedToDos) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("toDos", JSON.stringify(toDos));
+  }, [toDos]);
 
   const addToggle = () => {
     setTestCase(!testCase);
@@ -12,11 +19,20 @@ export default function ToDo() {
     title: "",
     dueDate: new Date().toISOString().split("T")[0],
     priority: "High",
+    checked: false,
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+  };
+
+  const handleChecked = (index) => {
+    setToDos(
+      toDos.map((task, i) =>
+        i === index ? { ...task, checked: !task.checked } : task
+      )
+    );
   };
 
   const handleSubmit = (e) => {
@@ -29,6 +45,7 @@ export default function ToDo() {
       title: "",
       dueDate: new Date().toISOString().split("T")[0], // just to convert the method into TODAY's date for default.
       priority: "High",
+      checked: false,
     });
 
     setToDos([...toDos, formData]);
@@ -65,7 +82,11 @@ export default function ToDo() {
                   <li key={index}>
                     <strong>{task.title}</strong> - {task.dueDate} -{" "}
                     {task.priority}
-                    <input type="checkbox" />
+                    <input
+                      type="checkbox"
+                      onChange={() => handleChecked(index)}
+                      checked={task.checked}
+                    />
                     <button onClick={() => deleteToDo(index)}>Delete</button>
                   </li>
                 );
